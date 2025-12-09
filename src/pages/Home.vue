283254d299f6425119786ae606a7aa168da28990
MrClaro <adryan.contatoprofissional@gmail.com>
2025-12-09 01:04:20 -0300

    feat(page): implementa página home da aplicação

---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<script setup>
+import BaseLayout from "../components/layout/BaseLayout.vue";
+import Header from "../components/layout/Header.vue";
+import Footer from "../components/layout/Footer.vue";
+
+import Banner from "../components/features/Banner.vue";
+import Inspire from "../components/features/Inspire.vue";
+</script>
+
+<template>
+  <BaseLayout>
+    <template #header><Header /></template>
+    <template #default>
+      <Banner />
+      <Inspire />
+    </template>
+    <template #footer><Footer /></template>
+  </BaseLayout>
+</template>
+
+<style scoped></style>
